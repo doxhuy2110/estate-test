d9f17bc24e392f0b7b187cd61e8c1b72c5dd1fa0
Huy <doxhuy2104@gmail.com>
2025-01-08 20:12:43 +0700

    sdfsdf

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -6,7 +6,6 @@ import { format } from "timeago.js";
 import { SocketContext } from "../../context/SocketContext";
 import { useNotificationStore } from "../../lib/notificationStore";
 
-
 function Chat({ chats }) {
   const [chat, setChat] = useState(null);
   const { currentUser } = useContext(AuthContext);
@@ -15,7 +14,6 @@ function Chat({ chats }) {
   const messageEndRef = useRef();
 
   const decrease = useNotificationStore((state) => state.decrease);
-
 
   useEffect(() => {
     messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -79,61 +77,54 @@ function Chat({ chats }) {
     <div className="chat">
       <div className="messages">
         <h1>Messages</h1>
-        {
-          chats.map((c) => (
-            <div className="message"
-              key={c.id}
-              style={{
-                backgroundColor:
-                  c.seenBy.includes(currentUser.id) || chat?.id === c.id
-                    ? "white"
-                    : "#fecd514e",
-              }}
-              onClick={() => handleOpenChat(c.id, c.receiver)}
-            >
-              <img
-                src={c.receiver.avatar || "/noavatar.png"}
-                alt=""
-              />
-              <span>{c.receiver.username}</span>
-              <p>{c.lastMessage}</p>
-            </div>
-          ))
-        }
-
+        {chats?.map((c) => (
+          <div
+            className="message"
+            key={c.id}
+            style={{
+              backgroundColor:
+                c.seenBy.includes(currentUser.id) || chat?.id === c.id
+                  ? "white"
+                  : "#fecd514e",
+            }}
+            onClick={() => handleOpenChat(c.id, c.receiver)}
+          >
+            <img src={c.receiver.avatar || "/noavatar.png"} alt="" />
+            <span>{c.receiver.username}</span>
+            <p>{c.lastMessage}</p>
+          </div>
+        ))}
       </div>
       {chat && (
         <div className="chatBox">
           <div className="top">
             <div className="user">
-              <img
-                src={chat.receiver.avatar || "noavatar.png"}
-                alt=""
-              />
+              <img src={chat.receiver.avatar || "noavatar.png"} alt="" />
               {chat.receiver.username}
             </div>
-            <span className="close" onClick={() => setChat(null)}>X</span>
+            <span className="close" onClick={() => setChat(null)}>
+              X
+            </span>
           </div>
           <div className="center">
-            {
-              chat.messages.map((message) => (
-                <div
-                  className="chatMessage"
-                  style={{
-                    alignSelf:
-                      message.userId === currentUser.id
-                        ? "flex-end"
-                        : "flex-start",
-                    textAlign:
-                      message.userId === currentUser.id ? "right" : "left",
-                  }}
-                  key={message.id}>
-                  <p>{message.text}</p>
-                  <span>{format(message.createdAt)}</span>
-                </div>
-              ))}
+            {chat.messages.map((message) => (
+              <div
+                className="chatMessage"
+                style={{
+                  alignSelf:
+                    message.userId === currentUser.id
+                      ? "flex-end"
+                      : "flex-start",
+                  textAlign:
+                    message.userId === currentUser.id ? "right" : "left",
+                }}
+                key={message.id}
+              >
+                <p>{message.text}</p>
+                <span>{format(message.createdAt)}</span>
+              </div>
+            ))}
             <div ref={messageEndRef}></div>
-
           </div>
           <form onSubmit={handleSubmit} className="bottom">
             <textarea name="text"></textarea>
