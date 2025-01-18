718dfb4f0eea5b2237f692db5eaf94ab18fc5e7f
Huy <doxhuy2104@gmail.com>
2025-01-18 23:54:13 +0700

    update

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
       <div className="left">
         <a href="/" className="logo">
           <img src="/logo.png" alt="" />
-          <span>LamaEstate</span>
+          <span>HiLife</span>
         </a>
         <a href="/">Home</a>
         <a href="/list">Post</a>
