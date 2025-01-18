5058411eb84ef92fd20503116537db07a51576f9
Huy <doxhuy2104@gmail.com>
2025-01-19 03:42:27 +0700

    1

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@prisma/client']
-    }
-  },
   server: {
     proxy: {
       '/api': {
